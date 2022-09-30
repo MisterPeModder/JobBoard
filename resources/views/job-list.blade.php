d5f2d2d2e75cc9a249d5697e6dd50b8e1d52f87b
Yanis Guaye <yanis.guaye@epitech.eu>
2022-09-30 08:29:44 +0000

    Add search fields and buttons

--- resources/views/job-list.blade.php
+++ resources/views/job-list.blade.php
@@ -12,22 +12,51 @@
 </head>
 
 <body class="antialiased bg-l-bgr-main">
-    <header
-        class="flex flex-row backdrop-blur-md sticky top-0 shadow-xl border-b border-l-brd/10 z-20 justify-between items-center p-2">
-        <span class="flex flex-row items-center gap-2">
-            <span class="logo-dot"></span>
-            <b id="logo" class="text-2xl">JobBoard</b>
-        </span>
-        <span class="flex flex-row gap-2">
-            @guest
-                {{-- "Sign In" widget, will only display when not logged --}}
-                <a href="#"
-                    class="bg-l-bgr-highlight text-white rounded-full p-1.5 text-sm flex items-center whitespace-nowrap font-semibold">
-                    Sign in</a>
-            @endguest
-            <img src="{{ Vite::asset('resources/images/hamburger.svg') }}" alt="menu">
+    <header class="flex flex-col backdrop-blur-md sticky top-0 border-b border-l-brd/10 z-20">
+        <span class="flex flex-row  justify-between items-center p-2">
+            <span class="flex flex-row items-center gap-2">
+                <span class="logo-dot"></span>
+                <a id="logo" href="/" class="text-2xl font-bold">JobBoard</a>
+            </span>
+            <span class="flex flex-row gap-2">
+                @guest
+                    {{-- "Sign In" widget, will only display when not logged --}}
+                    <a href="#"
+                        class="bg-l-bgr-highlight text-white rounded-full p-1.5 text-sm flex items-center whitespace-nowrap font-semibold">
+                        Sign in</a>
+                @endguest
+                <img src="{{ Vite::asset('resources/images/hamburger.svg') }}" alt="menu">
+            </span>
         </span>
     </header>
+    <div class="shadow-xl">
+        <div class="flex flex-row flex-wrap gap-2 w-11/12 mx-auto py-2 mt-2">
+            <div class="relative rounded-full shadow-sm w-full border border-inherit">
+                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+                    <img src="{{ Vite::asset('resources/images/search.svg') }}" alt="menu" class="">
+                </div>
+                <input type="text"
+                    class="bg-inherit block py-2 w-full rounded-full pl-10 pr-4 border-0 focus:outline-2 focus:outline-l-bgr-highlight focus:ring-l-bgr-highlight sm:text-sm"
+                    placeholder="What position are you looking for?"></input>
+            </div>
+            <div class="relative rounded-full shadow-sm w-full border border-inherit">
+                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+                    <img src="{{ Vite::asset('resources/images/location.svg') }}" alt="menu" class="">
+                </div>
+                <input type="text"
+                    class="bg-inherit block py-2 w-full rounded-full pl-10 pr-4 border-0 focus:outline-2 focus:outline-l-bgr-highlight focus:ring-l-bgr-highlight sm:text-sm"
+                    placeholder="Location"></input>
+            </div>
+            <a href="#"
+                class="bg-l-bgr-highlight text-white rounded-full p-1.5 text-sm flex items-center whitespace-nowrap font-semibold">
+                Find Jobs
+            </a>
+            <a href="#"
+                class="border-2 border-l-bgr-highlight text-l-bgr-highlight rounded-full p-1.5 text-sm flex items-center whitespace-nowrap font-semibold">
+                Filters
+            </a>
+        </div>
+    </div>
     <main class="container mx-auto py-2">
         <section id="adverts" class="flex flex-row flex-wrap gap-2 px-2">
             <x-job-advert title="Web 2.0 Developper" company="Web Systems Inc." location="Uranus (??)"
