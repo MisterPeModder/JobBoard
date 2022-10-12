998e0382151e717984ca062a03995f9d3c760a9b
MelvinCou <melvin.courjaud@epitech.eu>
2022-10-12 12:08:37 +0000

    Add status column to applications table

---
+++ database/migrations/2022_10_12_115553_add_application_status.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\ApplicationStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table
+                ->enum('status', ApplicationStatus::stringCases())->comment('Status of the apply')->default('new');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
