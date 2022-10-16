dfe19c0136598711626790bb9a38992893b3fe43
Yanis Guaye <yanis.guaye@epitech.eu>
2022-10-16 17:55:10 +0200

    Add french localization for admin assets menu

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,6 +1,6 @@
 <x-main-layout title="{{ __('admin.index.title') }}" :admin_link="false">
     <main class="mx-auto w-full md:w-10/12 lg:w-1/2 m-2 p-2 bg-l-bgr-content rounded-md border border-l-brd/10">
-        <h1 class="font-semibold text-xl">Admin dashboard</h1>
+        <h1 class="font-semibold text-xl">@tr('admin.index.title')</h1>
 
         <nav class="flex flex-row flex-wrap gap-2 justify-center border-t border-l-brd/10 pt-2 mt-2">
             <x-primary-link :admin="true" href="#">@tr('admin.applications.title')</x-primary-link>
@@ -11,7 +11,7 @@
         </nav>
 
         <section class="border-t border-l-brd/10 pt-2 mt-2">
-            <h2 class="font-semibold text-md">Statistics</h2>
+            <h2 class="font-semibold text-md">@tr('admin.statistics')</h2>
 
             <ul class="list-disc list-inside">
                 <li>{{ 'Adverts: ' . \App\Models\Advert::count() }}</li>
